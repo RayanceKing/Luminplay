274ae7c3197fbfd59114a00507824488fe2d10ca
RayanceKing <88320297+RayanceKing@users.noreply.github.com>
2026-04-26 10:41:19 +0800

    Initial Commit

---
+++ LuminplayTests/LuminplayTests.swift
@@ -0,0 +1,18 @@
+//
+//  LuminplayTests.swift
+//  LuminplayTests
+//
+//  Created by rayanceking on 2026/4/26.
+//
+
+import Testing
+
+struct LuminplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
